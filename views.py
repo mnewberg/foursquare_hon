10a910f192fb71d89b8f49a351847c46b8b889a1
Matt Newberg <matt@newbergfamily.net>
2012-01-19 23:55:49 -0500

    model issue

--- views.py
+++ views.py
@@ -101,16 +101,19 @@ def vote(request):
 	else:
 		r1 = rating(pic_id=pic_id, rating=1, total_sets=1)
 	r1.save()
-	record.objects.create(time=thetime, venue_id=venue_id, target=r1)
+	record1=record(time=thetime, venue_id=venue_id, target=r1)
+	record1.save()
 	if rating.objects.filter(pic_id=pic_id2).count()==1:
 		r2 = rating.objects.get(pic_id=pic_id2)
 		r2.total_sets +=1
 	else:        
 		r2 = rating(pic_id=pic_id2, rating=0, total_sets=1)
 	r2.save()
-	record.objects.create(time=thetime, venue_id=venue_id, target=r2)
+	record2=record(time=thetime, venue_id=venue_id, target=r2)
+	record2.save()
 	u1 = user.objects.get(fsq_id=request.session['fsq_id'])
-	u1.ratings.add(initial_record, second_record)
+	u1.ratings.add(record1)
+	u1.ratings.add(record2)
 	u1.save()
 	
 	return HttpResponse('Vote successful')
