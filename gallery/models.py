3a84b85440ccc2c150d7279327241b754f60499e
Matt Newberg <matt@newbergfamily.net>
2012-01-19 19:47:46 -0500

    more chgs

--- gallery/models.py
+++ gallery/models.py
@@ -18,7 +18,7 @@ class user(models.Model):
 	date_joined=models.DateTimeField()
 	first_name=models.CharField(max_length=20)
 	last_name=models.CharField(max_length=30)
-	ratings = models.ForeignKey(record)
+	ratings = models.ForeignKey('record')
 	phone = models.CharField(max_length=15)
 	twitter = models.CharField(max_length=30)
 	facebook = models.CharField(max_length=15)
