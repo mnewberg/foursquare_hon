6d4a53ae6df9b3148629cff4b3ed4de8b12b9104
Matt Newberg <matt@newbergfamily.net>
2012-01-19 19:10:36 -0500

    more chgs

--- gallery/models.py
+++ gallery/models.py
@@ -15,7 +15,7 @@ class record(models.Model):
 	
 class user(models.Model):
 	fsq_id=models.CharField(max_length=30)
-    date_joined=DateTimeField()
+	date_joined=models.DateTimeField()
 	first_name=models.CharField(max_length=20)
 	last_name=models.CharField(max_length=30)
 	ratings = models.ForeignKey(record)
