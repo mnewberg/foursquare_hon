17178c436a7e50c539b73148760e2e0ca6ce31d3
Matt Newberg <matt@newbergfamily.net>
2012-01-19 19:58:23 -0500

    more chgs

--- views.py
+++ views.py
@@ -73,7 +73,7 @@ def gallery(request):
 				if entry['user']['photo'][44:]=='':
 					pass
 				else:
-					chickpix[the_id]=[entry['user']['photo'][44:],entry['user']['firstName'],venueName,entry[id]]
+					chickpix[the_id]=[entry['user']['photo'][44:],entry['user']['firstName'],venueName,entry['id']]
 			else:
 				pass
 	rand_chickpix={}
