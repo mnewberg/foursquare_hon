8a7c63737969d65fffaed45dc554b5d9f5fac181
Matt Newberg <matt@newbergfamily.net>
2012-01-26 15:05:18 -0500

    adding venue rank

--- leaderboard.py
+++ leaderboard.py
@@ -5,7 +5,7 @@ from gallery.models import record, user
 def suggested_venues(fsq_id):
 	newdict = {}
 	for venue in record.objects.filter(time__lt=time(), time__gt=time()-3600, user=user.objects.get(fsq_id=fsq_id)).order_by('venue_id').values('venue_id'):
-		if item['venue_id'] not in newdict:
+		if venue['venue_id'] not in newdict:
 			newdict[venue['venue_id']]=1               
 		else:
 			newdict[venue['venue_id']]+=1
