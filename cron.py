536e7944e20902f5d2694a32ec3098e387b4eca7
Matt Newberg server <matt@newbergfamily.net>
2012-03-21 22:22:58 +0000

    fixed loading js

--- cron.py
+++ cron.py
@@ -7,6 +7,7 @@ import commands
 
 client = TwilioRestClient(settings.ACCOUNT_SID,settings.AUTH_TOKEN)
 to_expire=routing.objects.filter(time_created__lt=time()-1800)
+print to_expire
 for item in to_expire:
     client.sms.messages.create(to=item.recipient,from_=item.DID,body="*Message from Fourplay* Wrap it up! Your conversation will end in 5 minutes.")
     sleep(300)
