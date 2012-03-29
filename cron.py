2095c9eb3f1a815f7f950a42641b0ae9508862c1
Matt Newberg server <matt@newbergfamily.net>
2012-03-29 21:40:10 +0000

    fixed cron script

--- cron.py
+++ cron.py
@@ -10,7 +10,7 @@ to_expire=routing.objects.filter(time_created__lt=time()-1800)
 print to_expire
 for item in to_expire:
     client.sms.messages.create(to=item.recipient,from_=item.DID,body="*Message from Fourplay* Wrap it up! Your conversation will end in 5 minutes.")
-    sleep(300)
-    for item in to_expire:
-        item.delete()
+sleep(300)
+for item in to_expire:
+    item.delete()
 
