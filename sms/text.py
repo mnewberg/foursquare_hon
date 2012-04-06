29cd52abc10cb5527d2f6a792d2cc185aa4fea3c
Matt Newberg server <matt@newbergfamily.net>
2012-04-06 14:48:16 +0000

    fixed render to response

--- sms/text.py
+++ sms/text.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect
 from twilio.rest import TwilioRestClient
