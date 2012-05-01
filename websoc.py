896c1b7a5118e67e3807cafef45e72703b06a3ca
Matt Newberg server <matt@newbergfamily.net>
2012-05-01 10:13:57 -0600

    presence channel

--- websoc.py
+++ websoc.py
@@ -2,9 +2,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.utils import simplejson
 from django.shortcuts import render_to_response
-
+import json
 import pusher
 import settings
+from collections import OrderedDict
+
 
 pusher.app_id='19318'
 pusher.key='890abd57f862ab2712ff'
@@ -15,9 +17,19 @@ def auth(request):
     p = pusher.Pusher()
     socket = request.POST['socket_id']
     channel = request.POST['channel_name']
-    daresponse = p[channel].authenticate(socket)
-    return HttpResponse(simplejson.dumps(daresponse))
+    daresponse = p[channel].authenticate(socket,{'user_id':'mattnewb'})
+#    adict=OrderedDict([('user_id',10),('user_info',{'name':'Matt Newberg'})])
+    return HttpResponse(json.dumps(daresponse))
 
 def game(request):
     return render_to_response("game.html")
     
+@csrf_exempt
+def presence_auth(request):
+    p = pusher.Pusher()
+    socket = request.POST['socket_id']
+    channel = request.POST['channel_name']
+    daresponse = p[channel].authenticate(socket)
+    dict={'name':'matt newberg'}
+    daresponse['channel_data']=str(dict)
+    return HttpResponse(json.dumps(daresponse))
