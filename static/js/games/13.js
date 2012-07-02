37d7975ed524fab74cb2b4f1be6e12d80dfed179
Matt Newberg <matt@newbergfamily.net>
2012-07-02 17:51:54 -0400

    new game alert

---
+++ static/js/games/13.js
@@ -0,0 +1,9 @@
+var answers = {'Treats Truck':['treats'],'Wafels and Dinges':['waffles','wafles','wafels'],'Van Leeuwen':['van lu','van le'],
+'Big Gay Ice Cream':['big gay','gay ice'],'CoolHaus':['cool','kool'],'Korilla':['korila'],'Red Hook':['redhook'],
+'Frites N Meats':['frites'],'Taim Mobile':['taim','taiim'],'Calexico':[],'Mexicue':['mexi'],'Rickshaw Dumplings':['rickshaw','ricshaw'],
+'Cupcake Stop':['cupcake'],'La Cense':['la c'],'GO Burger':['go'],'MUD':['mud'],'Luke\'s Lobster':['luke','nauti'],
+'Gorilla Cheese':['gorilla','guerilla'],'Eddie\'s Pizza':['eddies'],'Phil\'s Steaks':['phil'],'DessertTruck':['dessert','desert'],'Schnitzel & Things':['schnitz','shnit']}
+var noun='truck'
+var title='Acclaimed food trucks in NYC'
+var cta='Can you list the most acclaimed food trucks in New York City?'
+var duration=240
---
+++ static/js/games/14.js
@@ -0,0 +1,5 @@
+var answers={'Caesar':['cesar'],'Bean':[],'Chef':[],'Chicken':[],'Chinese Chicken':['chineese chicken'],'Cobb':['cob'],'Coleslaw':['coeslaw','slaw','coslaw'],'Crab Louie':['crab louis'],'Egg':[],'Fruit':[],'Garden':[],'Greek':[],'Caprese':['capreese'],'Jello':['jelo'],'Nicoise':['niceois','neesoi','neswa','nicose'],'Tabbouleh':['taboule','tabboule','tabbule'],'Tuna':[],'Waldorf':[],'Mesclun':['mesculun','mesculin']}
+var noun='salad'
+var title='Names of salads'
+var cta='Can you name the most common salads in America?'
+var duration=210
