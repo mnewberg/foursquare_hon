1c6bf9a6ffe812cfcba9637567b7fdd39456c3e7
Matt Newberg <matt@newbergfamily.net>
2012-08-07 08:48:44 -0400

    integration test 1

---
+++ static/js/games/15.js
@@ -0,0 +1,7 @@
+var answers={'Cafe Grumpy':['grumpy'],'Abraco':[],'Think Coffee':['think'],'Teany':['teenie'],'Grey Dog':['gray dog','the grey','the gray'],'MUD':['mud'],'\'snice':['snice','nice'],
+'Stumptown':[],'Third Rail':['3rd'],'Joe':['jo'],'Bluebird':[],'La Colombe':[],'Ninth Street Espresso':['9th','ninth'],'Ost cafe':['ost'],'Saturdays Surf':['saturday'],'Ground Support':[],
+'Culture Espresso Bar':['culture'],'Gimme Coffe':['give','gimme','gime'],'Kaffe 1668':['kaffe','kafe'],'Everyman Espresso':['every']}
+var noun='cafe'
+var title='Most popular coffee spots in Manhattan'
+var cta='Name the most renowned coffee spots in Manhattan'
+var duration=200
