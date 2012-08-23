488e9a8f969b51ada663eeb7b3954e4ca8f7317f
Ryan Warner <warner.ryan@gmail.com>
2012-08-22 22:01:27 -0400

    gather info validation progress

--- static/javascript/gather_info.js
+++ static/javascript/gather_info.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+	 jQuery.validator.addMethod("phoneUS", function(phone_number, element) {
+    phone_number = phone_number.replace(/\s+/g, ""); 
+	return this.optional(element) || phone_number.length > 9 &&
+		phone_number.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
+}, "Please specify a valid phone number");
+
 	$('#venue, #tel').on('focus', function(){
 		$(this).next().html('');
 	});
@@ -15,4 +21,31 @@ $(document).ready(function() {
 			$(this).next().html('555-555-5555');
 		}
 	});
+
+	$("#gather_info_form").validate({
+		  rules: {
+		    twitter: {
+		      required: true,
+		    },
+		    tel: {
+		      required: true,
+		      phoneUS: true
+		    },
+		    venue: {
+		      required: true,
+		    }
+		  },
+
+		  errorPlacement: function(error, element) {
+		        error.insertBefore($(element).parent());
+		    }
+		});
+	$('#button').on('click', function(){
+
+		
+		$("#gather_info_form").valid();
+		
+
+	});
+	
 });
