338c0fcd02f18c127c89ce157f3f79754fcff562
Ryan Warner <warner.ryan@gmail.com>
2012-08-23 22:22:41 -0400

    animation junk

--- static/javascript/on_board_ui.js
+++ static/javascript/on_board_ui.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		game_animation();
 	});
 
-
+	runIt();
 });
 
 function twitter_bio(){
@@ -21,4 +21,16 @@ function game_animation(){
 	$('#screens').animate({
     	marginLeft: '0px'
     });
+}
+
+function runIt() {
+		setTimeout ( $('animation_text').html('P'), 0 );
+		setTimeout ( $('animation_text').html('Py'), 100 );
+		setTimeout ( $('animation_text').html('Pyr'), 200 );
+		setTimeout ( $('animation_text').html('Pyra'), 300 );
+		setTimeout ( $('animation_text').html('Pyram'), 400 );
+		setTimeout ( $('animation_text').html('Pyrami'), 500 );
+		setTimeout ( $('animation_text').html('Pyramid'), 600 );
+		setTimeout ( $('animation_text').html('Pyramids'), 700 );
+
 }
