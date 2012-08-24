1b65ac94d4067895d5098620a112d042815de93f
Matt Newberg server <matt@newbergfamily.net>
2012-08-24 08:47:32 -0600

    reverting

--- static/javascript/gather_info.js
+++ static/javascript/gather_info.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-	
-
 	 jQuery.validator.addMethod("phoneUS", function(phone_number, element) {
     phone_number = phone_number.replace(/\s+/g, ""); 
 	return this.optional(element) || phone_number.length > 9 &&
@@ -52,8 +50,4 @@ $(document).ready(function() {
 
 	});
 	
-});
-
-$(document).bind('DOMNodeInserted',function(){
-  	$('.ui-menu').appendTo('#venue-dropdown');
 });
