58dc7facbe55b097975625481687587c3259651f
Matt Newberg <matt@newbergfamily.net>
2012-08-28 17:08:31 -0400

    its working

--- static/javascript/on_board_ui.js
+++ static/javascript/on_board_ui.js
@@ -1,40 +1,93 @@
 $(document).ready(function() {
 
-	$('#arrow').on('click', function(){
+	$('#arrow').on('click', function() {
 		twitter_bio();
 	});
 
-	$('#back_arrow').on('click', function(){
+	$('#back_arrow').on('click', function() {
 		game_animation();
 	});
+	typing();
 });
 
-function twitter_bio(){
+var d=0
+var c=0
+
+
+function twitter_bio() {
 	$('#screens').animate({
-    	marginLeft: '-320px'
-    });
+		marginLeft: '-320px'
+	});
 }
 
-function game_animation(){
+function game_animation() {
 	$('#screens').animate({
-    	marginLeft: '0px'
-    });
+		marginLeft: '0px'
+	});
 }
 
 var text = "Guess an item"
-var delay= 1000
-i=0
-function text_animation(i){
-    $('#animation_text').html(word.slice(0,i));
+function theInterval(){
+		$('#animation_text')[0].innerHTML += text[d];
+		d++;
+		if (d >= text.length) {
+			clearInterval(the_typing);
+			runAnimation();
+		}
+	}
+
+function typing() {
+	 the_typing=setInterval(theInterval,500)
 }
 
-function runIt()
-{
-    var c = 0;
-    var interval = setInterval(function() { 
-        $('#animation_text')[0].innerHTML += text[c]; 
-        c++; 
-        if(c >= text.length) clearInterval(interval);
-    }, 300);
+var guesses = ['Mississippi', 'California', 'Nevada']
+
+function sample_guesses(){
+	$('#sample_guess').html(guesses[c]);
+	$('#your_score')[0].innerHTML++;
+	c++;
+	if (c >= guesses.length){
+		clearInterval(start_guess);
+	}
+}
+
+function scene_2() {
+	c=0
+	$('#your_score').html('0')
+	$('#sample_guess').html(guesses[c])
+	$('#scene_2').fadeIn('slow',function(){start_guess=setInterval(sample_guesses, 1000);})
+}
+
+function runAnimation() {
+	$('#scene_1').fadeOut('slow',scene_2());
+
+}
+
+var secs = 5;
+
+function timer() {
+    secs = secs - 1;
+    if (secs >= 0) {
+        $('#time_remaining')[0].innerHTML = '0:0' + secs;
+    } else {
+        clearInterval(the_timer);
+        $('#time_remaining').css({
+            color: 'red'
+        });
+        $('#scene_3').fadeOut('fast', function () {
+            $('#scene_1').fadeIn('fast');
+			$('#animation_text').html('')
+			d=0
+			$('#time_remaining').css({color: '#FFFC00'});		 
+			typing();
+        });
+    }
+}
+
+function scene_3() {
+	$('#scene_2').fadeOut('slow');
+	$('#scene_3').fadeIn('slow');
+	secs=5
+	the_timer=setInterval(timer, 1000);
 
 }
