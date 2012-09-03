b94bb69e41a33c40e96d04b07f0b8cd3228d5964
Matt Newberg server <matt@newbergfamily.net>
2012-09-03 15:03:57 -0600

    fixing bug in unicode?"

--- views.py
+++ views.py
@@ -208,7 +208,7 @@ def second(request):
       elif invite_codes.objects.filter(code=invite_code).count()==1:
 		request.session['fsq_id']=f_id
 		u=user.objects.create(fsq_id=query.id(), first_name=scrub(query.first_name()), last_name=scrub(query.last_name()),date_joined=datetime.datetime.today(),phone=query.phone(),email=query.email(),twitter=query.twitter(),facebook=query.facebook(),photo=query.photo()[36:], has_shared=False, token=token, gender=query.gender())
-                welcome_email(u.email(),u.first_name())
+                welcome_email(u.email,u.first_name)
                 authenticator.query("/users/6478/request",u.token,None)
                 invite_codes.objects.get(code=invite_code).delete()
                 return render_to_response('loc.html', {'lat':lat,'lon':lon,'sex':query.gender(),'token':token,'phone':query.phone(), 'twitter':query.twitter(),'recentcheckin':recentcheckin,'csrf':params}, context_instance=RequestContext(request))
@@ -219,7 +219,7 @@ def second(request):
                     photo=False
                 else:
                     photo=True
-                queue_email(query.email,query.first_name())
+                queue_email(query.email(),query.first_name())
                 return render_to_response('thank_you.html',{'photo':photo})
       else:
                 logger.error('Login exception', exc_info=True, extra={'stack': True})
